30853f624adca4e504a554af6f59a397bd97d68b
Ivan Benitez <ivanmatias300@gmail.com>
2025-05-08 16:58:28 -0300

    Fix: configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [
     react(),
     tailwindcss()
   ],
-})
+});
